32d3c7223b1f27c736b1efff30aad6e2f6b9c32c
Vitor <vitordeazambuja@edu.uniube.br>
2026-02-11 12:15:16 -0300

    chore: adiciona modelos de usuarios

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'usuario'
+
+    def __str__(self):
+        return self.username
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(
+        Usuario, 
+        on_delete=models.PROTECT
+    )
+    nome = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11, unique=True)
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=15)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.nome
+
+class PerfilDono(Perfil):
+    class Meta:
+        db_table = 'perfil_dono'
+
+class PerfilFuncionario(Perfil):
+    cargo = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'perfil_funcionario'
+
+    def __str__(self):
+        return f"{self.nome} - {self.cargo}"
