b72ee87b02358f450edbfa821393bf5d6aae9bf3
Vitor <vitordeazambuja@edu.uniube.br>
2026-02-11 15:24:38 -0300

    chore: cria views basicas de clinica e usuarios

--- backend/clinica/views.py
+++ backend/clinica/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Pet, Vacina, PetVacina
+from .serializers import PetSerializer, VacinaSerializer, PetVacinaSerializer
 
-# Create your views here.
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+    permission_classes = [IsAuthenticated]
+
+class VacinaViewSet(viewsets.ModelViewSet):
+    queryset = Vacina.objects.all()
+    serializer_class = VacinaSerializer
+    permission_classes = [IsAuthenticated]
+
+class PetVacinaViewSet(viewsets.ModelViewSet):
+    queryset = PetVacina.objects.all()
+    serializer_class = PetVacinaSerializer
+    permission_classes = [IsAuthenticated]
--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Usuario, PerfilDono, PerfilFuncionario
+from .serializers import UsuarioSerializer, PerfilDonoSerializer, PerfilFuncionarioSerializer
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+class PerfilDonoViewSet(viewsets.ModelViewSet):
+    queryset = PerfilDono.objects.all()
+    serializer_class = PerfilDonoSerializer
+    permission_classes = [IsAuthenticated]
+
+class PerfilFuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = PerfilFuncionario.objects.all()
+    serializer_class = PerfilFuncionarioSerializer
+    permission_classes = [IsAuthenticated]
