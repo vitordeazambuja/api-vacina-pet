1862496aae70884c6558c6142ada40e858e8d96f
Vitor <vitordeazambuja@gmail.com>
2026-02-13 21:54:01 -0300

    fix: remove rota inutilizada

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('api-auth/', include('rest_framework.urls')),
     
     path('api/v1/auth/', include('usuarios.urls')),
     path('api/v1/clinica/', include('clinica.urls')),
