f214e8affbb695fce33294aa81cee71f35785b5c
Vitor <vitordeazambuja@gmail.com>
2026-02-13 21:59:57 -0300

    chore: atualiza basenames de rotas e adiciona rotas de autenticacao

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UsuarioViewSet, PerfilDonoViewSet, PerfilFuncionarioViewSet
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import (
+    UsuarioViewSet, 
+    PerfilDonoViewSet, 
+    PerfilFuncionarioViewSet,
+    CustomTokenObtainPairView
+)
 
 router = DefaultRouter()
-router.register(r'users', UsuarioViewSet)
-router.register(r'donos', PerfilDonoViewSet)
-router.register(r'funcionarios', PerfilFuncionarioViewSet)
+router.register(r'users', UsuarioViewSet, basename='usuario')
+router.register(r'donos', PerfilDonoViewSet, basename='perfilusuario')
+router.register(r'funcionarios', PerfilFuncionarioViewSet, basename='perfilfuncionario')
 
 urlpatterns = [
+    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
